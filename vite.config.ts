d66ce7686f2713714423763fbd1fa396f13953c5
Alicia-Ouedraogo <ouedraogoalicia22@gmail.com>
2025-06-02 04:27:59 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/alicia-portfolio/',  // <-- ajoute cette ligne ici
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
